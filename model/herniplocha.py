f89fa283e91afa418b568d5df47ea87ffafb95e9
Gisbert W. Selke <gws@cpan.org>
2026-06-16 20:39:13 +0200

    Doc corrections; better error checking

--- model/herniplocha.py
+++ model/herniplocha.py
@@ -8,13 +8,14 @@ class HerniPlocha:
                   vyhozene_figurky_b : list[Figurka],
                   vyhozene_figurky_c : list[Figurka],
                   rozmery : tuple[int,int] = (8,8)
-                 ):
+                ):
         # self._herni_deska : list[list[Figurka]] = [ ]
         # Zkuste raději: self._herni_deska : dict[Pole,Figurka] = { }
         self.vyhozene_figurky_b = vyhozene_figurky_b.copy( ) # Nutné?
         self.vyhozene_figurky_c = vyhozene_figurky_c.copy( ) # Nutné?
-        self.rozmery = rozmery                               # Nutné? Jak o tom informovat poli?
-
+        if not 1 <= rozmery[0] <= 9 or not 1 <= rozmery[1] <= 9:
+            raise ValueError(f"Nesprávná rozměry plochy '{rozmery}'")
+        self.rozmery = rozmery                               # Nutné? Jak o tom informovat Pole?
 
     def vrat_obsah( self, pole: Pole ) -> Figurka:
         return ...
--- model/pole.py
+++ model/pole.py
@@ -1,21 +1,23 @@
+from typing import Self
+
 class Pole:
-    """Třída pro pozice na šachovnici."""
+    """Třída pro pole na šachovnici."""
 
     SLOUPCE = ' abcdefghij'
     RADY    = ' 123456789'
 
     def __init__(self, souradnice : str ):
         if len(souradnice) != 2:
-            raise ValueError(f"Illegal field id '{souradnice}'")
+            raise ValueError(f"Nesprávné souřadnice '{souradnice}'")
         self.souradnice = souradnice.lower( )
         self.sloupec    = self.SLOUPCE.find(self.souradnice[0])
         self.rada       = self.RADY.find(self.souradnice[1])
 
     @classmethod
-    def from_sloupec_rada( cls, s : int, r : int ) -> Pole:
+    def from_sloupec_rada( cls, s : int, r : int ) -> Self:
         if not 1 <= s <= 9 or not 1 <= r <= 9:
             raise ValueError(f"Coordinates out of bounds: ({s},{r})")
-        return Pole(f"{cls.SLOUPCE[s]}{cls.RADY[r]}")
+        return cls(f"{cls.SLOUPCE[s]}{cls.RADY[r]}")
 
     def __str__(self) -> str :
         return self.souradnice
